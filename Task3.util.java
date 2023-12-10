e599bd6d228f7872d48a909a4976abc50427c40d
EpoChip <ziop13@mail.ru>
2023-12-10 22:47:11 +0300

    Create Task3.util.java

---
+++ Task3.util.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите элементы массива через пробел: ");
+        String input = scanner.nextLine();
+        String[] inputArray = input.split("\\s+");
+
+        int[] array = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            array[i] = Integer.parseInt(inputArray[i]);
+        }
+
+        System.out.println("Элементы массива, кратные 3:");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
